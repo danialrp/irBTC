642e37cdc4602453fd0393351c9b3e8361ecbaef
Danial Rp <danialrz@icloud.com>
2015-09-13 02:19:45 +0430

    rate static done

---
+++ resources/views/partial/rate.blade.php
@@ -0,0 +1,22 @@
+<div class="rate-container">
+    <ul>
+        <li>
+            <span class="rate-up"></span>
+            <span class="rate-down"></span>
+            <span> بیتکوین/دلار </span>
+            <span class="numbers"> 235.65 </span>
+        </li>
+        <li>
+            <span class="rate-up"></span>
+            <span class="rate-down"></span>
+            <span> بیتکوین/تومان </span>
+            <span class="numbers"> 756,987 </span>
+        </li>
+        <li>
+            <span class="rate-up"></span>
+            <span class="rate-down"></span>
+            <span> بیتکوین/یورو </span>
+            <span class="numbers"> 208.34 </span>
+        </li>
+    </ul>
+</div>
--- resources/views/trade/bitcoin.blade.php
+++ resources/views/trade/bitcoin.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'مبادله بیتکوین')
 
 @section('content')
+    <div class="rate">
+        @include('/partial/rate')
+    </div>
+
     <div id="box">
         <div class="trade-box-right">
             <label class="trade-box-title"> فروش بیتکوین </label>
