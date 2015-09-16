0f23ca5267f8ace1d0f2d15d283d07b8d34df5a2
Danial Rp <danialrz@icloud.com>
2015-09-17 02:50:15 +0430

    fix-1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('inspire')
                  ->hourly();
 
-        $schedule->command('updateRate')
+        $schedule->command('irbtc:updateRate')
             ->everyMinute();
     }
 }
