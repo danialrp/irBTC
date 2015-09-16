6206614df23194008f2a6d4b253adfe20511f97a
Danial Rp <danialrz@icloud.com>
2015-09-17 03:12:16 +0430

    schedule for 5min

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,9 +26,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('inspire')
-                 ->hourly();
+            ->hourly();
 
         $schedule->command('irbtc:updateRate')
-            ->everyMinute();
+            ->everyFiveMinutes();
     }
 }
